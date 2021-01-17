38197dbc6ed6b202eb5643afb9f2d273da524668
Aditya Agarwal <adityaa803@gmail.com>
2021-01-18 04:05:03 +0530

    create mono-repo with separate pkgs like design-tokens & component lib

---
+++ packages/example/src/App.js
@@ -0,0 +1,15 @@
+import { Button } from '@dl/component-library';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>
+        It's alive
+      </Button>
+    </div>
+  );
+}
+
+export default App;
